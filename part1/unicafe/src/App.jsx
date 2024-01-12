6b5cc29d841d69f95c8d78672ae59a28392ba9fd
Aldo <60137518+aldojack@users.noreply.github.com>
2024-01-12 19:27:36 +0000

    1.7-1.11 - Added tables and added conditional rendering

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,16 +1,42 @@
 import { useState } from "react"
 
-const Heading = ({title}) => {
+const Heading = ({ title }) => {
   return <h1>{title}</h1>
 }
 
-const Button = ({name, onClick}) => {
+const Button = ({ name, onClick }) => {
   return <button onClick={() => onClick(name)}>{name}</button>
 }
 
-const Stat = ({stat, count}) => {
-  return <p>{stat}: {count}</p>
+const Stat = ({ stat, count }) => {
+  return (
+    <tr>
+      <td><p>{stat}: {count}{stat === 'Positive' && '%'}</p></td>
+    </tr>
+  )
 }
+
+const Table = ({ heading, good, bad, neutral }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>{heading}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <Stat stat="Good" count={good} />
+        <Stat stat="Neutral" count={neutral} />
+        <Stat stat="Bad" count={bad} />
+        <Stat stat="All" count={good + bad + neutral} />
+        <Stat stat="Average" count={((good + bad + neutral) / 3)} />
+        <Stat stat="Positive" count={((good / (good + bad + neutral)))} />
+      </tbody>
+    </table>
+  )
+}
+
+
 
 
 function App() {
@@ -19,7 +45,7 @@ function App() {
   const [bad, setBad] = useState(0)
 
   const handleIncrease = (type) => {
-    switch(type){
+    switch (type) {
       case 'good':
         setGood(good + 1);
         break;
@@ -30,19 +56,22 @@ function App() {
         setBad(bad + 1);
         break;
     }
-    
+
   }
 
   return (
     <div>
-    <Heading title="Give Feedback"/>
-      <Button name="good" onClick={handleIncrease}/>
-      <Button name="neutral" onClick={handleIncrease}/>
-      <Button name="bad" onClick={handleIncrease}/>
-      <Heading title="Statistics"/>
-      <Stat stat="Good" count={good}/>
-      <Stat stat="Neutral" count={neutral}/>
-      <Stat stat="Bad" count={bad}/>
+      <Heading title="Give Feedback" />
+      <Button name="good" onClick={handleIncrease} />
+      <Button name="neutral" onClick={handleIncrease} />
+      <Button name="bad" onClick={handleIncrease} />
+      {good + bad + neutral === 0 ? <p>No feedback given</p> :
+        (
+          <Table heading="Statistics" good={good} bad={bad} neutral={neutral}/>
+        )
+      }
+
+
 
     </div>
   )
