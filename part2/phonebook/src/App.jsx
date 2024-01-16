281ab5c968173b47e4b322fb2f04b1c459a14308
Alan Jack <60137518+aldojack@users.noreply.github.com>
2024-01-16 13:40:58 +0000

    2.8 - App now takes in number and validates not a duplicate

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,51 +1,64 @@
 import { useState } from "react";
+import "./app.css";
 
 function App() {
-  const [people, setPeople] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const [people, setPeople] = useState([
+    { name: "Arto Hellas", number: "+447445121" },
+  ]);
+  const [newPerson, setNewPerson] = useState({ name: "", number: "" });
 
   const handleChange = (event) => {
-    setNewName(event.target.value);
-    
+    const { name, value } = event.target;
+    setNewPerson({ ...newPerson, [name]: value });
   };
 
   const handleAdd = (event) => {
     event.preventDefault();
-    const newPerson = {name: newName}
-    const alreadyExists = people.find(person => person.name === newPerson.name);
-    if(!alreadyExists){
-      setPeople(people.concat(newPerson));
+    const addNewPerson = {...newPerson};
+    const alreadyExists = people.find(
+      (person) => person.name === addNewPerson.name || person.number === addNewPerson.number
+    );
+    if (!alreadyExists) {
+      setPeople(people.concat(addNewPerson));
+    } else {
+      alert(`${addNewPerson.name} is already added to phonebook`);
     }
-    else{
-      alert(`${newPerson.name} is already added to phonebook`)
-    }
-  }
+  };
 
-  const validateData = (data) => {
-    console.log(data);
-    const allPeople = new Set({...people});
-    return allPeople.has(data)
-  }
 
   const renderedPeople = people.map((person) => (
-    <p key={person.name}>{person.name}</p>
+    <p key={person.name}>
+      {person.name}: {person.number}
+    </p>
   ));
 
   return (
     <div>
-      <h2>Phonebook</h2>
       <form onSubmit={handleAdd}>
-        <label htmlFor="nameInput">name:</label>
-        <input
-          type="text"
-          id="nameInput"
-          name="nameInput"
-          value={newName}
-          onChange={handleChange}
-        />
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <fieldset>
+          <legend>Phonebook</legend>
+          <div className="form-group">
+            <label htmlFor="nameInput">name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={newPerson.name}
+              onChange={handleChange}
+            />
+            <div className="form-group">
+              <label htmlFor="numberInput">number:</label>
+              <input
+                type="text"
+                id="number"
+                name="number"
+                value={newPerson.number}
+                onChange={handleChange}
+              />
+            </div>
+            <button type="submit">add</button>
+          </div>
+        </fieldset>
       </form>
       <h2>Numbers</h2>
       <div>{renderedPeople}</div>
