036db4110839eb8ffef270c7767970105e55556a
Alan Jack <60137518+aldojack@users.noreply.github.com>
2024-01-17 11:09:01 +0000

    2.9 - Added filter functionallity

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,21 +3,43 @@ import "./app.css";
 
 function App() {
   const [people, setPeople] = useState([
-    { name: "Arto Hellas", number: "+447445121" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newPerson, setNewPerson] = useState({ name: "", number: "" });
+  const [filter, setFilter] = useState("");
+  const [filteredPeople, setFilteredPeople] = useState([]);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     setNewPerson({ ...newPerson, [name]: value });
   };
 
+  const handleFilter = (event) => {
+    const { value } = event.target;
+    setFilter(value);
+    filterContacts(value);
+  };
+
+  const filterContacts = (filteredName) => {
+    const filteredPeople = [...people].filter((person) =>
+      person.name.toLowerCase().includes(filteredName)
+    );
+    setFilteredPeople(filteredPeople);
+  };
+
   const handleAdd = (event) => {
     event.preventDefault();
-    const addNewPerson = {...newPerson};
-    const alreadyExists = people.find(
-      (person) => person.name === addNewPerson.name || person.number === addNewPerson.number
-    );
+    const addNewPerson = { ...newPerson, id: people.length + 1 };
+    const alreadyExists = people.find((person) => {
+      return (
+        person.name === addNewPerson.name ||
+        person.number === addNewPerson.number
+      );
+    });
+
     if (!alreadyExists) {
       setPeople(people.concat(addNewPerson));
     } else {
@@ -25,20 +47,32 @@ function App() {
     }
   };
 
-
-  const renderedPeople = people.map((person) => (
-    <p key={person.name}>
-      {person.name}: {person.number}
-    </p>
-  ));
+  const renderedList = (peopleList) => {
+    return peopleList.map((people) => (
+      <p key={people.id}>
+        {people.name}: {people.number}
+      </p>
+    ));
+  };
 
   return (
-    <div>
+    <div id="main">
+      <div className="form-group">
+        <label htmlFor="search">Filter by name:</label>
+        <input
+          type="text"
+          id="search"
+          name="search"
+          value={filter}
+          onChange={handleFilter}
+        />
+      </div>
+      <h2>Add New</h2>
       <form onSubmit={handleAdd}>
         <fieldset>
           <legend>Phonebook</legend>
           <div className="form-group">
-            <label htmlFor="nameInput">name:</label>
+            <label htmlFor="name">name:</label>
             <input
               type="text"
               id="name"
@@ -47,7 +81,7 @@ function App() {
               onChange={handleChange}
             />
             <div className="form-group">
-              <label htmlFor="numberInput">number:</label>
+              <label htmlFor="number">number:</label>
               <input
                 type="text"
                 id="number"
@@ -60,8 +94,8 @@ function App() {
           </div>
         </fieldset>
       </form>
-      <h2>Numbers</h2>
-      <div>{renderedPeople}</div>
+      <h2>Contacts</h2>
+      <div>{!filter ? renderedList(people) : renderedList(filteredPeople)}</div>
     </div>
   );
 }
