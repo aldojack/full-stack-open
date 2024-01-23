31799dea6a542f4b131da6c7e9b6ad7208d6bb8a
Alan Jack <60137518+aldojack@users.noreply.github.com>
2024-01-23 12:18:33 +0000

    CRUD functionallity added to contact module

--- part2/phonebook/src/services/contacts.js
+++ part2/phonebook/src/services/contacts.js
@@ -1,9 +1,23 @@
-import axios from 'axios'
-const baseUrl = "http://localhost:3001/contacts"
+import axios from "axios";
+const baseUrl = "http://localhost:3001/contacts";
 
 const getAll = () => {
-    const request = axios.get(baseUrl);
-    return request.then(response => response.data);
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const update = (id, updatedContact) => {
+  const request = axios.put(`${baseUrl}/${id}`, updatedContact);
+  return request.then((response) => response.data);
+};
+
+const deleteContact = (id) => {
+  return axios.delete(`${baseUrl}/${id}`);
 }
 
-export default {getAll}
+const create = (newContact) => {
+  const request = axios.post(`${baseUrl}`, newContact);
+  return request.then((response) => response.data);
+};
+
+export default { getAll, create, deleteContact, update };
