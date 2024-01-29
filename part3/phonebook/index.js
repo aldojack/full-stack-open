ca9ad6292b7ed652b648604ee7f2568d4567750f
Alan Jack <60137518+aldojack@users.noreply.github.com>
2024-01-29 15:51:37 +0000

    3.4: Phonebook backend step4
    Extracted db.json to a variable using spread syntax
    
    Refactored code to reflect the variable
    
    Added delete end point which handles status codes based on successful delete or no matching ID
    
    Also implemented delete of person if id exists on database

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,24 +5,41 @@ const PORT = 3001;
 
 app.use(express.json());
 
+let personDb = [...data]
+
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
 app.get("/info", (req, res) => {
   const body = `
-        <p>Phonebook has info for ${data.length} people</p>
+        <p>Phonebook has info for ${personDb.length} people</p>
         <p>${new Date().toString()}</p>`;
   res.status(200).send(body);
 });
 /* start of persons endpoint */
 app.get("/api/persons", (req, res) => {
-  res.json(data);
+  console.log(personDb);
+  res.json(personDb);
 });
 
 app.get("/api/persons/:id", (req, res) => {
   const id = +req.params.id;
-  const person = data.find((person) => person.id === id);
+  const person = personDb.find((person) => person.id === id);
   if (!person) res.status(404).send(`No records found for ID ${id}`);
   else res.status(200).json(person);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = +req.params.id;
+
+  const personToDelete = personDb.find((person) => person.id === id);
+
+  if (!personToDelete) res.status(404).send(`No records found for ID ${id}`);
+  else {
+    // personDb = personDb.filter(person => person.id !== personToDelete.id)
+    const findPerson = personDb.indexOf(personToDelete)
+    personDb.splice(findPerson,1);
+    res.status(204).end();
+  };
 });
 
 /* end of persons endpoint */
