bba4330a293b9505f87fe4975ea1744f6cb57692
Alan Jack <60137518+aldojack@users.noreply.github.com>
2024-01-29 13:21:58 +0000

    3.3: Phonebook backend step3
    Implemented get end point for individual people using parameters and error handling for when no match

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,19 +1,28 @@
-const express = require('express');
-let data = require('./db.json');
+const express = require("express");
+let data = require("./db.json");
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
-app.get('/api/persons', (req, res) => {
-    res.json(data);
+app.get("/info", (req, res) => {
+  const body = `
+        <p>Phonebook has info for ${data.length} people</p>
+        <p>${new Date().toString()}</p>`;
+  res.status(200).send(body);
+});
+/* start of persons endpoint */
+app.get("/api/persons", (req, res) => {
+  res.json(data);
 });
 
-app.get('/info',(req, res) => {
-    const body = `
-        <p>Phonebook has info for ${data.length} people</p>
-        <p>${new Date().toString()}</p>`
-    res.status(200).send(body);
-})
+app.get("/api/persons/:id", (req, res) => {
+  const id = +req.params.id;
+  const person = data.find((person) => person.id === id);
+  if (!person) res.status(404).send(`No records found for ID ${id}`);
+  else res.status(200).json(person);
+});
 
-
+/* end of persons endpoint */
