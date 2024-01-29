c0054049b333d653e8a4d1aecab403e3fd6f634a
Alan Jack <60137518+aldojack@users.noreply.github.com>
2024-01-29 08:04:50 +0000

    3.2: Phonebook backend step2
    Simple exercise, create 'info' route that returns a html body showing the number of people and todays date

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -7,5 +7,13 @@ app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
 app.get('/api/persons', (req, res) => {
     res.json(data);
+});
+
+app.get('/info',(req, res) => {
+    const body = `
+        <p>Phonebook has info for ${data.length} people</p>
+        <p>${new Date().toString()}</p>`
+    res.status(200).send(body);
 })
+
 
