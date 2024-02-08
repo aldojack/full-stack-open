1820a56e98a73b92d04236b234c2a46c94854a8c
Alan Jack <60137518+aldojack@users.noreply.github.com>
2024-02-08 11:18:34 +0000

    Exercise 3.22
    installed eslint and implemented single quotes and semi colons in the code with 2 indents for consistency

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const Person = require("./model/person");
+const express = require('express');
+const Person = require('./model/person');
 const app = express();
-const morgan = require("morgan");
-const cors = require("cors");
+const morgan = require('morgan');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.use(express.static("dist"));
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(cors());
-morgan.token("body", (req, res) => {
-  if (req.method === "POST") {
+morgan.token('body', (req, res) => {
+  if (req.method === 'POST') {
     return JSON.stringify(req.body);
   }
 });
@@ -23,16 +23,16 @@ app.use(
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
       tokens.body(req, res),
-    ].join(" ");
+    ].join(' ');
   })
 );
 
-app.get("/info", async (req, res) => {
+app.get('/info', async (req, res) => {
   let totalPeople = await Person.find({}).then((results) => {
     return results.length;
   });
@@ -43,7 +43,7 @@ app.get("/info", async (req, res) => {
 });
 
 /* start of persons endpoint */
-app.get("/api/persons", (req, res, next) => {
+app.get('/api/persons', (req, res, next) => {
   Person.find({})
     .then((collection) => {
       res.json(collection);
@@ -51,13 +51,13 @@ app.get("/api/persons", (req, res, next) => {
     .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (req, res, next) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then((personFound) => res.json(personFound))
     .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res, next) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
     .then((result) => {
       res.status(204).end();
@@ -65,7 +65,7 @@ app.delete("/api/persons/:id", (req, res, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res, next) => {
+app.post('/api/persons', (req, res, next) => {
   const newPerson = new Person({
     ...req.body,
   });
@@ -79,7 +79,7 @@ app.post("/api/persons", (req, res, next) => {
     .catch((error) => next(error));
 });
 
-app.put("/api/persons/:id", (req, res, next) => {
+app.put('/api/persons/:id', (req, res, next) => {
   const updatedPerson = {
     ...req.body,
   };
@@ -95,11 +95,11 @@ app.put("/api/persons/:id", (req, res, next) => {
 
 const errorHandler = (error, request, response, next) => {
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
-  } else if (error.name === "StrictModeError") {
+  } else if (error.name === 'StrictModeError') {
     return response.status(400).json({ error: error.message });
   }
 
@@ -107,7 +107,7 @@ const errorHandler = (error, request, response, next) => {
 };
 
 const unknownEndpoints = (req, res) => {
-  res.status(404).send({ error: "Unknown endpoint" });
+  res.status(404).send({ error: 'Unknown endpoint' });
 };
 app.use(errorHandler);
 app.use(unknownEndpoints);
--- part3/phonebook/model/person.js
+++ part3/phonebook/model/person.js
@@ -1,39 +1,39 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI).catch((err) => {
-  if (err.code === 8000) console.log(`Incorrect password`);
+  if (err.code === 8000) console.log('Incorrect password');
 });
 
-console.log("Connected");
+console.log('Connected');
 
 const personSchema = new mongoose.Schema(
   {
     name: {
       type: String,
       minLength: 3,
-      required: [true, "Name required"],
+      required: [true, 'Name required'],
     },
     number: {
       type: String,
-      required: [true, "User phone number required"],
+      required: [true, 'User phone number required'],
       validate: {
         validator: function (v) {
           return /\d{2,3}-\d{6}/.test(v);
         },
         message: (props) => {
           if(props.value.length >= 8){
-            return `${props.value} is not a valid phone number. It must be like ${props.value.split('').toSpliced(2,0,'-').join('')} or ${props.value.split('').toSpliced(3,0,'-').join('')}`
+            return `${props.value} is not a valid phone number. It must be like ${props.value.split('').toSpliced(2,0,'-').join('')} or ${props.value.split('').toSpliced(3,0,'-').join('')}`;
           }
         },
       },
       minLength: 8,
     },
   },
-  { strict: "throw" }
+  { strict: 'throw' }
 );
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
@@ -41,4 +41,4 @@ personSchema.set("toJSON", {
   },
 });
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema);
