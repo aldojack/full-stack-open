205c88ae02aca21039b9a7c125d996d0e757020e
Alan Jack <60137518+aldojack@users.noreply.github.com>
2024-02-11 18:25:11 +0000

    4.5*: Helper Functions and Unit Tests, step 3
    Moved the blog posts data into stub data module to keep testing code clear
    
    Now have testing function for dummy, total likes and favourite blog
    
    jest test file implements the helper function and dummy data

--- part4/blog/test/dummy.test.js
+++ part4/blog/test/dummy.test.js
@@ -1,8 +1,32 @@
 const listHelper = require('../utils/list_helper')
+const data = require('../utils/stub_data')
 
 test('dummy returns one', () => {
-  const blogs = []
-
-  const result = listHelper.dummy(blogs)
+  const result = listHelper.dummy(data.listWithZero)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+
+
+    test('of empty list is zero', () => {
+        const result = listHelper.totalLikes(data.listWithZero)
+        expect(result).toBe(0)
+    })
+
+    test('when list has only one blog equals the likes of that', () => {
+        
+        const result = listHelper.totalLikes(data.listWithOneBlog)
+        expect(result).toBe(5)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const result = listHelper.totalLikes(data.listWithMultipleBlogs)
+        expect(result).toBe(36)
+    })
+})
+
+  test('most liked blog', () => {
+    const result = listHelper.favoriteBlog(data.listWithMultipleBlogs)
+    expect(result).toEqual(data.listWithMultipleBlogs[2])
+  })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,7 +1,23 @@
 const dummy = (blogs) => {
     return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+}
+
+const totalLikes = (blogs) => {
+    if (blogs.length <= 0) return 0
+
+    let totalLikes = 0
+
+    blogs.forEach(blog => {
+        totalLikes += blog.likes
+    })
+
+    return totalLikes
+}
+
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((fav, blog) => fav.likes > blog.likes ? fav : blog)
+}
+
+module.exports = {
+    dummy, totalLikes, favoriteBlog
+}
