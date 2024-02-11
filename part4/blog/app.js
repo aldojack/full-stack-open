99641fca592765a71213420a7fb15a8b3848a224
Alan Jack <60137518+aldojack@users.noreply.github.com>
2024-02-11 11:13:45 +0000

    4.2 Blog List, step 2
    Got the blog app up and running as per 4.1 by connecting to database and installing and importing required dependences
    
    4.2 slowly moving code into modules required for the specific functionallity with a model for the Schema, controller for the routes, app and index to start the whole thing

---
+++ part4/blog/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const blogsRouter = require('./controller/blogs')
+const {MONGODB_URI} = require('./utils/config')
+const cors = require('cors')
+
+mongoose.connect(MONGODB_URI)
+.then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
