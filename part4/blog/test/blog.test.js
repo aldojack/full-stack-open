1f77c1b18197d0d105459be8769f9ff644319b74
Alan Jack <60137518+aldojack@users.noreply.github.com>
2024-03-08 13:43:45 +0000

    Remove dummy testing

--- part4/blog/test/blog.test.js
+++ part4/blog/test/blog.test.js
@@ -1,10 +1,6 @@
 const listHelper = require('../utils/list_helper')
 const data = require('../utils/stub_data')
 
-test('dummy returns one', () => {
-  const result = listHelper.dummy(data.listWithZero)
-  expect(result).toBe(1)
-})
 
 describe('total likes', () => {
 
--- part4/blog/test/dummy.test.js
+++
@@ -1,7 +0,0 @@
-const listHelper = require('../utils/list_helper')
-const data = require('../utils/stub_data')
-
-test('dummy returns one', () => {
-  const result = listHelper.dummy(data.listWithZero)
-  expect(result).toBe(1)
-})
