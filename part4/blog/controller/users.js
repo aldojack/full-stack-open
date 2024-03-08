11670b0c62bfbca8d6325bf0de541567869dd241
Alan Jack <60137518+aldojack@users.noreply.github.com>
2024-03-08 13:46:48 +0000

    POST users method added
    usernames will remove any strings and also error handling for passwords under 3 characters

--- part4/blog/controller/users.js
+++ part4/blog/controller/users.js
@@ -8,11 +8,19 @@ usersRouter.get('/', async (request, response) => {
 })
 
 usersRouter.post('/', async (request, response) => {
-    const {username, name, password} = request.body
+    let { username, name, password } = request.body
+    username = username.replace(/\s+/g, '');
+    
+    if (password.length < 3) {
+        return response.status(400).json({
+            error: 'Password length too short must be greater than 3'
+        })
+    }
+
     const saltRounds = 10
     const hashedPassword = await bcrypt.hash(password, saltRounds)
 
-    const user = new User({username, name, hashedPassword})
+    const user = new User({ username, name, hashedPassword })
     const savedUser = await user.save()
     response.status(201).json(savedUser)
 })
