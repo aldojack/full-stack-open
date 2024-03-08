7e333f77b79479acd86db2a3a8f139090d39eb14
Alan Jack <60137518+aldojack@users.noreply.github.com>
2024-03-08 13:45:04 +0000

    Update user model to have min length for username and must be unique, also both username and password are required

--- part4/blog/model/user.js
+++ part4/blog/model/user.js
@@ -1,20 +1,28 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    name: String, 
-    hashedPassword: String
+  username: {
+    type: String,
+    unique: true,
+    minLength: 3,
+    required: true
+  },
+  name: String,
+  hashedPassword: { 
+    type: String, 
+    required: true 
+  }
 })
 
 userSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-      delete returnedObject.hashedPassword
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.hashedPassword
 
-    }
-  })
+  }
+})
 
 module.exports = mongoose.model('User', userSchema)
 
