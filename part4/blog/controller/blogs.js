2e1b830e36354d321bbbf26e87d67495acc1c076
Alan Jack <60137518+aldojack@users.noreply.github.com>
2024-03-20 11:19:14 +0000

    Blog router decodes the token to verify it is from the valid user who owns the token

--- part4/blog/controller/blogs.js
+++ part4/blog/controller/blogs.js
@@ -1,49 +1,63 @@
 const blogsRouter = require('express').Router()
-const {blogErrorHandling} = require('../utils/middleware')
+const { blogErrorHandling } = require('../utils/middleware')
 const Blog = require('../model/blog')
 const User = require('../model/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
 
   const blogs = await Blog.find({}).populate('user', { 'username': 1, 'name': 1 })
   response.json(blogs)
-  })
-  
+})
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
-    const {body} = request
-    const user = await User.findById(body.userId)
-    if(!user) return response.status(400).end()
-
-    const blogToBeAdded = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url, 
-      likes: body.likes || 0,
-      user: body.userId
-    })
-
-    const savedBlog = await blogToBeAdded.save()
-    
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-
-    response.status(201).json(savedBlog)
+  const { body } = request
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  console.log(decodedToken)
+  if(!decodedToken.id){
+    return response.status(401).json({error: "Token invalid"})
+  }
+  const user = await User.findById(decodedToken.id)
+  if (!user) return response.status(400).end()
+  console.log('user: ', user)
+  const blogToBeAdded = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+    user: user._id
   })
 
-  blogsRouter.delete('/:id', async(request, response) => {
-    const {id} = request.params
-    await Blog.findByIdAndDelete(id)
-    response.sendStatus(204)
-  })
+  const savedBlog = await blogToBeAdded.save()
 
-  blogsRouter.put('/:id', async (request, response) => {
-    const {id} = request.params
-    const {likes} = request.body
-    
-    if(!likes || likes <0) throw new Error('Missing likes property')
-    const blogToUpdate = await Blog.findByIdAndUpdate(id, {likes: likes}, {new: true, runValidators: true})
-    response.status(200).json(blogToUpdate)
-  })
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params
+  await Blog.findByIdAndDelete(id)
+  response.sendStatus(204)
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { id } = request.params
+  const { likes } = request.body
+
+  if (!likes || likes < 0) throw new Error('Missing likes property')
+  const blogToUpdate = await Blog.findByIdAndUpdate(id, { likes: likes }, { new: true, runValidators: true })
+  response.status(200).json(blogToUpdate)
+})
 
 
-  module.exports = blogsRouter
+module.exports = blogsRouter
